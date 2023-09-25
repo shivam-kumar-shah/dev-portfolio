1895357a395fdda7dba21226a24505bb5323f04c
shivam-kumar-shah <shivam-kumar-shah@outlook.com>
2023-09-25 17:51:12 +0530

    refactor: :art: add profile section

--- app/components/backend/index.tsx
+++ app/components/backend/index.tsx
@@ -14,6 +14,7 @@ import { Page } from '../layout/Page';
 import { Main } from '../layout/Main';
 import { SkillsPane } from '../layout/SkillsPane';
 import { ProjectsPane } from '../layout/ProjectsPane';
+import { Project } from '../project';
 
 export const Backend = () => {
   const [selectedIndex, setSelectedIndex] = useState(-1);
@@ -64,53 +65,56 @@ export const Backend = () => {
     <Page id={'backend'}>
       <SubHeading className='text-center'>and a</SubHeading>
       <Heading className='text-center text-6xl'>Backend Web Developer</Heading>
-      <Main>
-        <SkillsPane carousel={carousel}>
-          <div
-            className='backend-skills grid grid-cols-3 grid-rows-2 gap-4 transition-all ease-in-out'
-            ref={ref}
-          >
-            <SkillTile showAnimation={showAnimation}>
-              <DiNodejs size={'100%'} color='rgb(103, 187, 74)' />
-            </SkillTile>
-            <SkillTile showAnimation={showAnimation}>
-              <SiExpress size={'100%'} color='white' />
-            </SkillTile>
-            <SkillTile showAnimation={showAnimation}>
-              <DiMongodb size={'100%'} color='rgb(0, 230, 97)' />
-            </SkillTile>
-            <SkillTile showAnimation={showAnimation}>
-              <SiMysql size={'100%'} color='rgb(0, 113, 139)' />
-            </SkillTile>
-            <SkillTile showAnimation={showAnimation}>
-              <SiPostman size={'100%'} color='rgb(231, 106, 61)' />
-            </SkillTile>
-            <SkillTile showAnimation={showAnimation}>
-              <SiGraphql size={'100%'} color='rgb(222, 51, 166)' />
-            </SkillTile>
-          </div>
-        </SkillsPane>
-        <ProjectsPane>
-          <ProjectTile
-            bringToFocus={() => bringToFocus(0)}
-            projectType={'Express JS MongoDB'}
-            resetFocus={resetFocus}
-            showAnimation={showAnimation}
-            title={'posts api'}
-            className={'animated-tile-1 text-darkTextPrimary'}
-            key={'posts api'}
-          />
-          <ProjectTile
-            bringToFocus={() => bringToFocus(1)}
-            projectType={'Express JS Typescript'}
-            resetFocus={resetFocus}
-            showAnimation={showAnimation}
-            title={'bing news api'}
-            className={'animated-tile-2 text-darkTextPrimary'}
-            key={'bing news api'}
-          />
-        </ProjectsPane>
-      </Main>
+      <div className='main flex h-full w-full flex-row'>
+        <Main className='w-0'>
+          <SkillsPane carousel={carousel}>
+            <div
+              className='backend-skills grid grid-cols-3 grid-rows-2 gap-4 transition-all ease-in-out'
+              ref={ref}
+            >
+              <SkillTile showAnimation={showAnimation}>
+                <DiNodejs size={'100%'} color='rgb(103, 187, 74)' />
+              </SkillTile>
+              <SkillTile showAnimation={showAnimation}>
+                <SiExpress size={'100%'} color='white' />
+              </SkillTile>
+              <SkillTile showAnimation={showAnimation}>
+                <DiMongodb size={'100%'} color='rgb(0, 230, 97)' />
+              </SkillTile>
+              <SkillTile showAnimation={showAnimation}>
+                <SiMysql size={'100%'} color='rgb(0, 113, 139)' />
+              </SkillTile>
+              <SkillTile showAnimation={showAnimation}>
+                <SiPostman size={'100%'} color='rgb(231, 106, 61)' />
+              </SkillTile>
+              <SkillTile showAnimation={showAnimation}>
+                <SiGraphql size={'100%'} color='rgb(222, 51, 166)' />
+              </SkillTile>
+            </div>
+          </SkillsPane>
+          <ProjectsPane>
+            <ProjectTile
+              bringToFocus={() => bringToFocus(0)}
+              projectType={'Express JS MongoDB'}
+              resetFocus={resetFocus}
+              showAnimation={showAnimation}
+              title={'posts api'}
+              className={'animated-tile-1 text-darkTextPrimary'}
+              key={'posts api'}
+            />
+            <ProjectTile
+              bringToFocus={() => bringToFocus(1)}
+              projectType={'Express JS Typescript'}
+              resetFocus={resetFocus}
+              showAnimation={showAnimation}
+              title={'bing news api'}
+              className={'animated-tile-2 text-darkTextPrimary'}
+              key={'bing news api'}
+            />
+          </ProjectsPane>
+        </Main>
+        <Project />
+      </div>
     </Page>
   );
 };
---
+++ app/components/project/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Main } from '../layout/Main';
+
+type Props = {};
+
+export const Project = (props: Props) => {
+  return <Main className=' bg-red-500 md:h-4/5'>Hello World</Main>;
+};
